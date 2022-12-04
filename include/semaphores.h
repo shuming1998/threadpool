80adfd94a5b3235caf78ac490f15ac141e098b54
cuishuming <cuishuming@trunk.tech>
2022-12-04 14:05:23 +0800

    solve semaphore deconstruct

--- include/semaphores.h
+++ include/semaphores.h
@@ -2,19 +2,21 @@
 #define SEMAPHORES_H
 
 #include <mutex>
+#include <atomic>
 #include <condition_variable>
 
 // 基于互斥锁和条件变量实现的信号量类，用于线程通信
 class Semaphore {
 public:
   Semaphore(int limit = 0);
-  ~Semaphore() = default;
+  ~Semaphore();
   // 获取一个信号量资源
   void wait();
   // 增加一个信号量资源
   void post();
 
 private:
+  std::atomic_bool isExit_;
   int resLimit_;
   std::mutex mtx_;
   std::condition_variable cond_;
--- src/semaphore.cpp
+++ src/semaphore.cpp
@@ -1,10 +1,17 @@
 #include "semaphores.h"
 
-Semaphore::Semaphore(int limit) : resLimit_(limit) {}
+Semaphore::Semaphore(int limit) : resLimit_(limit), isExit_(false) {}
 
+Semaphore::~Semaphore() {
+  isExit_ = true;
+}
 
 // 获取一个信号量资源
 void Semaphore::wait() {
+  //! 如果信号量资源已经析构，就直接返回
+  if (isExit_) {
+    return;
+  }
   std::unique_lock<std::mutex> lock(mtx_);
   // 阻塞等待，直到信号量有资源
   cond_.wait(lock, [&]() { return resLimit_ > 0; });
@@ -13,7 +20,15 @@ void Semaphore::wait() {
 
 // 增加一个信号量资源
 void Semaphore::post() {
+  //! 如果信号量资源已经析构，就直接返回
+  if (isExit_) {
+    return;
+  }
   std::unique_lock<std::mutex> lock(mtx_);
   ++resLimit_;
+  //! 如果用局部 result 对象接收 submitTask 的结果，result 对象析构后，result 中的
+  //! 成员 semaphore 析构 =》semaphore 中的 condition_variable 析构
+  //! 而 Linux下 condition_variable 的析构函数为空函数，没有释放资源
+  //! notify_all 会失效，任务线程会阻塞在这里
   cond_.notify_all();
 }
